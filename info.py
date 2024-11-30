720014ba20c8d88ac3857b2ae447f6d7194885ec
Swordigo <140787949+swordigo007@users.noreply.github.com>
2024-12-01 00:46:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "26452259"))
+API_HASH     = environ.get("API_HASH", "21aae51e1ce8afa9e0781ee39a03ead1")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002280047787"))
+ADMIN        = int(environ.get("ADMIN", "1604373715"))
+CHANNEL      = environ.get("CHANNEL", "@multiflixpremium")
