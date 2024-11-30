dff56754090237bd556d96e52a8c5061f68d644c
Swordigo <140787949+swordigo007@users.noreply.github.com>
2024-12-01 01:39:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,7 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "26452259"))
 API_HASH     = environ.get("API_HASH", "21aae51e1ce8afa9e0781ee39a03ead1")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "7412166799:AAFnK8Zv-nu844LxSpr-wad-HlDgwsIN2n0")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://khalkarhrishikesh:Hpk@2001@mflix.7esjb.mongodb.net/?retryWrites=true&w=majority&appName=Mflix")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002280047787"))
 ADMIN        = int(environ.get("ADMIN", "1604373715"))
